88354643c49253073302cbed5a72c5d30524ff1c
MatthewGimbut <mattgimbut@gmail.com>
2017-02-08 13:32:01 -0500

    Added cartracker.js
    Set up some preliminary changes to allow to testing of the API once
    access is granted.

---
+++ js/cartracker.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Matthew on 2/7/2017.
+ */
+
+function searchCarInfo() {
+    console.log("This should probably do something in the future");
+    //TODO Do something
+
+    //var make = $("#makeInput").value;
+    var make = $("#makeinput").val();
+    var model = $("#modelInput").val();
+    var year = $("#yearInput").val();
+
+    console.log(make);
+    console.log(model);
+    console.log(year);
+
+}
