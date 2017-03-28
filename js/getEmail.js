349cfcdbe85c6c1dd37410bc23b29125626d49d3
Joe Mecca <jdmecca812@yahoo.com>
2017-03-28 00:01:01 -0400

    testing database implementation for retrieving the email automatically

--- js/getEmail.js
+++ js/getEmail.js
@@ -19,8 +19,21 @@ function initializeEmailJS(){
  * @param toEmail target Email
  */
 function sendTestNotification(){
+    var params = {tloEmai: userJSON.email};
     initializeEmailJS();
-    emailjs.send("default_service","notification", html(userJSON.email));
+
+    // Change to your service ID, or keep using the default service
+    var service_id = "default_service";
+
+    var template_id = "notification";
+    src="https://cdn.emailjs.com/dist/email.min.js";
+    src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js";
+    emailjs.send(service_id,template_id,params)
+        .then(function(){
+            alert("Sent!");
+        }, function(err) {
+            alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+        });
 }
 
 function sendTestNotif(){
