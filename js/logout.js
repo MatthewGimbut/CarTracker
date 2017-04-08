7793e643effc3a979a5dc4229fefb85450ffa8a0
mikecrinite <mikecrinite@gmail.com>
2017-04-08 16:36:27 -0400

    User cookie is deleted on "logout"

---
+++ js/logout.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Mike on 4/8/2017.
+ */
+
+$(document).ready(function() {
+    $("#logout").click(function(){
+        alert("clicked");
+        localStorage.setItem('userJSON', null);
+    });
+});
