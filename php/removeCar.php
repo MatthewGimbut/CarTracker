da5fe6646fc75bb34bfcb099ea1208b15eceb60c
mikecrinite <mikecrinite@gmail.com>
2017-04-14 02:29:50 -0400

    Used right variable in the php file....

--- php/removeCar.php
+++ php/removeCar.php
@@ -5,7 +5,7 @@ $mysqli = new mysqli('mikedb.clzedg3q1dlc.us-west-2.rds.amazonaws.com','MikeDB',
 $carID = $_GET["carID"];
 
 $stmt = $mysqli->prepare('DELETE FROM cars WHERE carID=?') or die($mysqli->error);
-$stmt->bind_param("i", $userID);
+$stmt->bind_param("i", $carID);
 $stmt->execute();
 
 $stmt->close();
