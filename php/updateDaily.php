d015b7d040be4bb19e85bc8812860c35a6783a07
mikecrinite <mikecrinite@gmail.com>
2017-04-15 16:45:22 -0400

    Added script to query database and send mail to users. Currently only sends to my own account as I am the only one in the database set up for daily updates

---
+++ php/updateDaily.php
@@ -0,0 +1,34 @@
+<?php
+
+$mysqli = new mysqli('mikedb.clzedg3q1dlc.us-west-2.rds.amazonaws.com','MikeDB','moscariello', 'CarTrackerInfo');
+
+$userstmt = $mysqli->prepare('SELECT email,userID FROM users WHERE users.notifPref="daily"') or die($mysqli->error);
+$userstmt->execute();
+$users = $userstmt->get_result();
+
+//Go through users whose emails were on the daily list
+while($row = $users->fetch_assoc()) {
+  $email = $row['email'];
+  $userID = $row['userID'];
+
+  $carsstmt = $mysqli->prepare('SELECT year,make,model,mileage,mileageLastInspection FROM cars WHERE userID=?') or die($mysqli->error);
+  $carsstmt->bind_param("i", $userID);
+  $cars = $carsstmt->get_result();
+  $str = "";
+
+  while($car = $cars->fetch_assoc()){
+      $since = $car['mileage'] - $car['mileageLastInspection'];
+      $str = $str . $car['year'] . $car['make'] . $car['model'] . ": " . $since . " miles since maintenance\n";
+
+      //Execute the python script
+      exec('python ../python/send_mail.py $email $str');
+  }
+}
+
+
+$users->close();
+$userstmt = null;
+$cars->close();
+$carsstmt = null;
+$mysqli->close();
+?>
