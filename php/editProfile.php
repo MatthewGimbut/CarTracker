db49f311358892dc5a709becc1e1e4a22ea6f43a
genovaandrew <genovaandrew@gmail.com>
2017-04-16 19:57:22 -0400

    Removed all debugging failures
    :(

--- php/editProfile.php
+++ php/editProfile.php
@@ -1,11 +1,4 @@
 <?php
-function debug_to_console( $data ) {
-    $output = $data;
-    if ( is_array( $output ) )
-        $output = implode( ',', $output);
-
-    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
-}
 
 $mysqli = new mysqli('mikedb.clzedg3q1dlc.us-west-2.rds.amazonaws.com', 'MikeDB', 'moscariello', 'CarTrackerInfo');
 
@@ -21,8 +14,6 @@ $result = $stmt->get_result();
 while ($row = $result->fetch_assoc()) {
     $userID = $row['userID'];
 }
-
-debug_to_console( "firstest checkpoint" );
 
 //get all other possibly changed data
 $firstName = $_GET["firstName"];
@@ -66,13 +57,9 @@ if($bYear !== null){
     $statement->close();
 }
 
-debug_to_console( "first checkpoint" );
-
 $stmt = $mysqli->prepare("SELECT * FROM users WHERE userID = $userID");
 $stmt->execute();
 $result = $stmt->get_result();
-
-debug_to_console( "second" );
 
 while ($row = $result->fetch_assoc()) {
     $myArray[] = $row;
@@ -80,8 +67,5 @@ while ($row = $result->fetch_assoc()) {
 
 $mysqli->close();
 
-debug_to_console( "third" );
-
 echo $_GET['callback'] . '(' . json_encode($myArray) . ')';
 ?>
-
