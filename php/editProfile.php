7ac2ae5e582ef8d928a53cc910d5fb15cef1e36c
genovaandrew <genovaandrew@gmail.com>
2017-04-15 22:23:57 -0400

    Corrected html based on fields users are allowed to edit

--- php/editProfile.php
+++ php/editProfile.php
@@ -1,17 +1,23 @@
 <?php
-    $mysqli = new mysqli('mikedb.clzedg3q1dlc.us-west-2.rds.amazonaws.com','MikeDB','moscariello', 'CarTrackerInfo');
 
-    //get the user's ID from the database
-    $username = $_GET["username"];
-    $userID = 0;
+$mysqli = new mysqli('mikedb.clzedg3q1dlc.us-west-2.rds.amazonaws.com', 'MikeDB', 'moscariello', 'CarTrackerInfo');
 
-    $stmt = $mysqli->prepare("SELECT userID FROM users WHERE users.username=?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $result = $stmt->get_result();
+//get the user's ID from the database
+$username = $_GET["username"];
+$userID = 0;
 
-    while($row = $result->fetch_assoc()){
-        $userID = $row['userID'];
-    }
+$stmt = $mysqli->prepare("SELECT userID FROM users WHERE users.username=?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$result = $stmt->get_result();
 
+while ($row = $result->fetch_assoc()) {
+    $userID = $row['userID'];
+}
+
+while ($row = $result->fetch_assoc()) {
+    $myArray[] = $row;
+}
+
+echo $_GET['callback'] . '(' . json_encode($myArray) . ')';
 ?>
