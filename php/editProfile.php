542ec95c398c3fea11fb5879b8c7806d61018a9e
genovaandrew <genovaandrew@gmail.com>
2017-04-15 20:40:23 -0400

    Get user id

---
+++ php/editProfile.php
@@ -0,0 +1,17 @@
+<?php
+    $mysqli = new mysqli('mikedb.clzedg3q1dlc.us-west-2.rds.amazonaws.com','MikeDB','moscariello', 'CarTrackerInfo');
+
+    //get the user's ID from the database
+    $username = $_GET["username"];
+    $userID = 0;
+    
+    $stmt = $mysqli->prepare("SELECT userID FROM users WHERE users.username=?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    while($row = $result->fetch_assoc()){
+        $userID = $row['userID'];
+    }
+
+?>
