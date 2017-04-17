40a5d96e39270a1a7a499dbc38dc3666e645f786
Joe Mecca <jdmecca812@yahoo.com>
2017-04-17 00:21:05 -0400

    reverted file to before last commit

--- js/createUser.js
+++ js/createUser.js
@@ -17,232 +17,89 @@ $(document).ready(function(){
         var bMonth = $("#bMonth").val();
         var bYear = $("#bYear").val();
 
-        if(validateFirstName(firstName) && validateLastName(lastName) && validateUsername(username)){
-            if (validateEmail(email, confirmEmail)) {
-                if (validatePassword(password, confirmPassword)) {
-                    if (validateDOB(bDay, bMonth, bYear)) {
-
-                        $.ajax({
-                            async: false,
-                            type: 'GET',
-                            url: '../php/postUser.php',
-                            dataType: 'jsonP',
-                            contentType: 'application/javascript',
-                            jsonp: 'callback',
-                            jsonpcallback: 'logResults',
-                            data: {
-                                firstName: firstName,
-                                lastName: lastName,
-                                username: username,
-                                email: email,
-                                password: password,
-                                bDay: bDay,
-                                bMonth: bMonth,
-                                bYear: bYear
-                            },
-                            success: function (response, textStatus) {
-                                console.log(textStatus);
-                                console.log(JSON.stringify(response));
-
-                                saveCookies(JSON.stringify(response));
-                                window.open("../pages/userProfile.html", "_self");
-                            },
-                            error: function (jqXHR, textStatus, errorThrown) {
-                                alert("Error " + errorThrown + "\nPlease contact the webmaster with this error.");
-                            }
-                        })
-
-                        //sending verification email, since everything is A-OKAY
-                        sendVerificationEmail(email);
-
-                    }
-                    else{
-                        //validateDOB
-                        alert("Invalid birth date! (Must be 13 years or older to register!)");
-                    }
+        if(validateNames(firstName, lastName, username)
+            && validateEmail(email, confirmEmail)
+            && validatePassword(password, confirmPassword)
+            && validateDOB(bDay, bMonth, bYear)){
+            $.ajax({
+                async: false,
+                type: 'GET',
+                url: '../php/postUser.php',
+                dataType: 'jsonP',
+                contentType:'application/javascript',
+                jsonp: 'callback',
+                jsonpcallback: 'logResults',
+                data: {firstName: firstName,
+                    lastName: lastName,
+                    username: username,
+                    email: email,
+                    password: password,
+                    bDay: bDay,
+                    bMonth: bMonth,
+                    bYear: bYear},
+                success: function(response, textStatus){
+                    console.log(textStatus);
+                    console.log(JSON.stringify(response));
+                    saveCookies(JSON.stringify(response));
+                    //send verification email
+                    window.open("../pages/userProfile.html", "_self");
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    alert("Error " + errorThrown + "\nPlease contact the webmaster with this error.");
                 }
-                else{
-                    //validatePassword
-                    alert("Error: Check your password and the confirmation!\nValid characters for a password are:" +
-                        "\n    -Lowercase or capital letters\n    -Digits\n    -!@#$%^&*" +
-                        "\nPasswords must be between 8 and 16 characters and contain one of each of the above.");
-                }
-            }
-            else{
-                //validateEmail
-                alert("Error:Check the email field and its confirmation field!\nNot a valid e-mail address");
-            }
+            })
+
+            //sending verification email
+            sendVerificationEmail(email);
         }
-        else{
-            //validateFirstName, lastName, username
-            alert("Error:\n-Fields cannot be blank.\n-First Name and Username must be 20 characters or less.\n" +
-                "-Last Name must be 30 characters or less \n-First and last names can only contain letters\n" +
-                "-Username can only contain letters, numbers, underscores and dashes.");
-        }
+
     });
 });
 
-$("#firstName").keyup(function(){
-    var firstName = $('#firstName').val();
-
-    if(!validateFirstName(firstName)){
-        $("#firstName").css("background-color", "Tomato")
-    }
-    else{
-        $("#firstName").css("background-color", "Chartreuse")
-    }
-});
-
-$("#lastName").keyup(function(){
-    var lastName = $('#lastName').val();
-
-    if(!validateLastName(lastName)){
-        $("#lastName").css("background-color", "Tomato")
-    }
-    else{
-        $("#lastName").css("background-color", "Chartreuse")
-    }
-});
-
-$("#username").keyup(function(){
-    var username = $('#username').val();
-
-    if(!validateUsername(username)){
-        $("#username").css("background-color", "Tomato")
-    }
-    else{
-        $("#username").css("background-color", "Chartreuse")
-    }
-});
-
-$("#email").keyup(function(){
-    var email = $("#email").val();
-    var confirmEmail = $("#confirmEmail").val();
-
-    if(!validateEmail(email, confirmEmail)) {
-        $("#email").css("background-color", "Tomato");
-        $("#confirmEmail").css("background-color", "Tomato");
-    }
-    else{
-        $("#email").css("background-color", "Chartreuse");
-        $("#confirmEmail").css("background-color", "Chartreuse");
-    }
-});
-
-
-$("#confirmEmail").keyup(function(){
-    var email = $("#email").val();
-    var confirmEmail = $("#confirmEmail").val();
-
-    if(!validateEmail(email, confirmEmail)) {
-        $("#email").css("background-color", "Tomato");
-        $("#confirmEmail").css("background-color", "Tomato");
-    }
-    else{
-        $("#email").css("background-color", "Chartreuse");
-        $("#confirmEmail").css("background-color", "Chartreuse");
-    }
-});
-
-$("#password").keyup(function(){
-    var password = $("#password").val();
-    var confirmPassword = $("#confirmPassword").val();
-
-    if(!validatePassword(password, confirmPassword)) {
-        $("#password").css("background-color", "Tomato");
-        $("#confirmPassword").css("background-color", "Tomato");
-    }
-    else{
-        $("#password").css("background-color", "Chartreuse");
-        $("#confirmPassword").css("background-color", "Chartreuse");
-    }
-});
-
-$("#confirmPassword").keyup(function(){
-    var password = $("#password").val();
-    var confirmPassword = $("#confirmPassword").val();
-
-    if(!validatePassword(password, confirmPassword)) {
-        $("#password").css("background-color", "Tomato");
-        $("#confirmPassword").css("background-color", "Tomato");
-    }
-    else{
-        $("#password").css("background-color", "Chartreuse");
-        $("#confirmPassword").css("background-color", "Chartreuse");
-    }
-});
-
-
-
 
 /**
- * Checks whether entered first name is valid
+ * Checks whether entered names are valid
  *
  * @param firstName Must contain either letters (currently English alphabet only) or spaces
- * @returns {boolean} True if all tests pass
- */
-function validateFirstName(firstName){
-    firstName = firstName.trim();
-
-    var regex = /^[a-zA-Z ]+$/; //Lower, upper, space character
-
-    //Check for blank
-    if(firstName === "" || firstName.length > 20){
-        return false;
-    }
-
-    if(!regex.test(firstName)) {
-
-        return false;
-    }
-
-    return true;
-}
-
-/**
- * Checks whether entered last name is valid
- *
  * @param lastName Must contain either letters (currently English alphabet only) or spaces
- * @returns {boolean} True if all tests pass
- */
-function validateLastName(lastName){
-    lastName = lastName.trim();
-
-    var regex = /^[a-zA-Z ]+$/; //Lower, upper, space character
-
-    //Check for blank
-    if(lastName === "" || lastName.length > 30){
-        return false;
-    }
-
-    if(!regex.test(lastName)) {
-        return false;
-    }
-
-    return true;
-}
-
-/**
- * Checks whether entered username is valid
- *
  * @param username Must contain either letters, digits, underscore or hyphen
  * @returns {boolean} True if all tests pass
  */
-function validateUsername(username){
+function validateNames(firstName, lastName, username){
+    firstName = firstName.trim();
+    lastName = lastName.trim();
     username = username.trim();
-    var regex = /^[a-zA-Z0-9_-]+$/; //Lower, upper, digits, underscore, hyphen
+    var regex = /^[a-zA-Z ]+$/; //Lower, upper, space character
 
-    if(username === "" || username.length > 20){
+    //Check for blank
+    if(firstName === ""
+            || firstName.length > 20
+            || lastName === ""
+            || lastName.length > 30
+            || username === ""
+            || username.length > 20 ){
+        alert("Error:\nFields cannot be blank.\nFirst Name and Username must be 20 characters or less.\n" +
+            "Last Name must be 30 characters or less");
         return false;
     }
 
+    //Check first name, last name for valid characters
+    if(!regex.test(firstName)
+        || !regex.test(lastName)){
+        console.log(firstName + ", " + lastName);
+        alert("Error:\nFirst Name or Last Name contains invalid characters");
+        return false;
+    }
+
+    regex = /^[a-zA-Z0-9_-]+$/; //Lower, upper, digits, underscore, hyphen
+
     if(!regex.test(username)){
+        alert("Error:\nInvalid username");
         return false;
     }
 
     return true;
 }
-
 
 /**
  * Determines whether password matches its confirmation, and whether it is valid.
@@ -258,8 +115,12 @@ function validatePassword(password, confirmPassword){
     var regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
 
     if (password !== confirmPassword) {
+        alert("Error:\nPasswords do not match!");
         return false;
     }else if(!regex.test(password)){
+        alert("Error:\nValid characters for a password are:" +
+            "\n    -Lowercase or capital letters\n    -Digits\n    -!@#$%^&*" +
+            "\nPasswords must be between 8 and 16 characters and contain one of each of the above.");
         return false;
     }
     return true;
@@ -276,30 +137,33 @@ function validateEmail(email, confirmEmail) {
     regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (email !== confirmEmail) {
-        // alert("Error:\nEmails do not match");
+        alert("Error:\nEmails do not match");
         return false;
     } else if (!regex.test(email)) {
+        alert("Error:\nNot a valid e-mail address");
         return false;
     }
     return true;
 }
-
 
 function validateDOB(bDay, bMonth, bYear){
     var date = new Date();
 
     if(bDay <= 31 && bDay > 0){
         if(bMonth <= 12 && bMonth > 0){
-            if(bYear <= (date.getFullYear() - 13) && bYear >= 1900){
+            if(bYear <= (date.getFullYear() - 13) && bYear > 1900){
                 return true;
             }
             else{
+                alert("Year not in range!");
                 return false;
             }
         }else{
+            alert("Month not in range!");
             return false;
         }
     }else{
+        alert("Day not in range!");
         return false;
     }
 }
