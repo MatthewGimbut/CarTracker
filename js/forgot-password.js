3bd655a7b5fc7bdc689ca5e399a552c9b23b2452
oconnells0 <oconnells0@students.rowan.edu>
2017-04-17 13:41:28 -0400

    Finished Forgot Password
    Now generates a random password, updates the password to the database,
    emails it to the user and instructs user to manually change password
    later, brings to login page

--- js/forgot-password.js
+++ js/forgot-password.js
@@ -8,23 +8,64 @@ $(document).ready(function(){
         var bMonth = $("#bMonth").val();
         var bYear = $("#bYear").val();
 
-        alert(
+
+        //testing
+        /*alert(
             "Email: " + email +
             "\nB Day: " + bDay +
             "\nB Month: " + bMonth +
             "\nB Year: " + bYear
-        );
+        );*/
 
         if(validateEmail(email) && validateDOB(bDay,bMonth,bYear)) {
-            alert("good");
+            //alert("good");
             //data valid
             //  if there is a user with this email and DOB
-            //      login to that user
-            //      take to reset-password.html
+            //      generate random temp password
+            //      change password to the temp password
+            //      email the user with the temp password
             //  esle notify that user is not found
+            newPassword = generateRandomPassword();
+            var emailMessage = "Your temporary password is " + newPassword +
+                "\nPlease log in using this temporary password and go to user profile page and manually change your password";
+            $.ajax({
+                async: false,
+                type: 'GET',
+                url: '../php/forgot-password.php',
+                //url: '//localhost/forgot-password.php',
+                dataType: 'jsonP',
+                contentType:'application/javascript',
+                jsonp: 'callback',
+                jsonpcallback: 'logResults',
+                data: {
+                    email: email,
+                    bDay: bDay,
+                    bMonth: bMonth,
+                    bYear: bYear,
+                    newPassword: newPassword
+                },
+                success: function(response, textStatus){
+                    console.log(textStatus);
+                    if(response.length > 0) {
+                        //found the user, can reset password
+                        console.log(textStatus);
+                        console.log(JSON.stringify(response));
+                        //sendTestNotification();
+                        sendAlertNotification(email,emailMessage,0,"Password");
+                        alert("An Email Has Been Sent to " + email + " With A Temporary Password");
+                        window.open("../pages/login.html", "_self");
+                    }
+                    else {
+                        alert("user with that email and birth date not found");
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    alert("Error " + errorThrown + "\n       Please contact the webmaster with this error.");
+                }
+            })
         }
     });
-})
+});
 
 function validateEmail(email) {
     regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -56,4 +97,24 @@ function validateDOB(bDay, bMonth, bYear){
         alert("Day not in range!\nMust be between 1 and 31");
         return false;
     }
+}
+
+function generateRandomPassword() {
+    var text = "";
+    var capitol = "ABCDEFGHJKMNPQRSTUVWXYZ";
+    var lower = "abcdefghjkmnpqrstuvwxyz";
+    var number = "123456789";
+    var symbol = "@#$%^&";
+    text += capitol.charAt(Math.floor(Math.random() * capitol.length));
+    text += lower.charAt(Math.floor(Math.random() * lower.length));
+    text += number.charAt(Math.floor(Math.random() * number.length));
+    text += symbol.charAt(Math.floor(Math.random() * symbol.length));
+    text += capitol.charAt(Math.floor(Math.random() * capitol.length));
+    text += lower.charAt(Math.floor(Math.random() * lower.length));
+    text += number.charAt(Math.floor(Math.random() * number.length));
+    text += symbol.charAt(Math.floor(Math.random() * symbol.length));
+    text += capitol.charAt(Math.floor(Math.random() * capitol.length));
+    text += lower.charAt(Math.floor(Math.random() * lower.length));
+
+    return text;
 }
