b8ffac8beb47d9b3a4d6a4dfc3671618ae62c32e
Michael Crinite <mikecrinite@gmail.com>
2017-04-17 14:53:54 -0400

    fixed this dumb mistake

--- php/setVerification.php
+++ php/setVerification.php
@@ -4,7 +4,7 @@ $mysqli = new mysqli('mikedb.clzedg3q1dlc.us-west-2.rds.amazonaws.com','MikeDB',
 
 $username = $_GET["username"];
 
-$stmt = $mysqli("UPDATE users SET verified=1 WHERE username=?");
+$stmt = $mysqli->prepare("UPDATE users SET users.verified=1 WHERE username=?");
 $stmt->bind_param("s", $username);
 $stmt->execute();
 $stmt->close();
