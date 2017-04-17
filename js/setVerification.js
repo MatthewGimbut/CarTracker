b9bb53e0c99bcf956741f9ab11a73063ace4c03d
Joe Mecca <jdmecca812@yahoo.com>
2017-04-17 01:07:12 -0400

    added verification stages for the user going to the confirmation link

---
+++ js/setVerification.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Joe Mecca on 4/17/2017.
+ */
+
+var userJSON;
+
+function verifyEmail(){
+    $.ajax({
+        async: false,
+        type: 'GET',
+        //url: '../php/setVerification.php',
+        url: '//localhost/setVerification.php',
+        dataType: 'jsonP',
+        contentType:'application/javascript',
+        jsonp: 'callback',
+        jsonpcallback: 'logResults',
+        data: {
+            username: username,
+            verified: 1
+        },
+        success: function(response, textStatus){
+            console.log(textStatus);
+            console.log(JSON.stringify(response));
+            alert("You have been verified!");
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("Error " + errorThrown + "\nPlease contact the webmaster with this error.");
+        }
+    })
+}
---
+++ php/setVerification.php
@@ -0,0 +1,8 @@
+<?php
+
+$mysqli = new mysqli('mikedb.clzedg3q1dlc.us-west-2.rds.amazonaws.com','MikeDB','moscariello', 'CarTrackerInfo');
+
+$username = $_GET["username"];
+UPDATE users SET verified=1 WHERE username=$username;
+
+?>
