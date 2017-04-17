2bc3e2e6453461bbcd5ce7164cd5d0b9f97a6461
Michael Crinite <mikecrinite@gmail.com>
2017-04-17 14:18:38 -0400

    php updates verified = 1

--- php/setVerification.php
+++ php/setVerification.php
@@ -3,7 +3,11 @@
 $mysqli = new mysqli('mikedb.clzedg3q1dlc.us-west-2.rds.amazonaws.com','MikeDB','moscariello', 'CarTrackerInfo');
 
 $username = $_GET["username"];
-'UPDATE users SET verified=1 WHERE username=$username';
+
+$stmt = $mysqli("UPDATE users SET verified=1 WHERE username=?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+$stmt->close();
 
 $mysqli->close();
 
